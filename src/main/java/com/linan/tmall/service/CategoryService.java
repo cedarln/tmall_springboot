00069bf80cf660b80eed4437f995fb461b2d6656
cedarln <cedarln@foxmail.com>
2021-11-02 15:55:22 +0800

    增加分页功能

--- src/main/java/com/linan/tmall/service/CategoryService.java
+++ src/main/java/com/linan/tmall/service/CategoryService.java
@@ -2,7 +2,11 @@ package com.linan.tmall.service;
 
 import com.linan.tmall.dao.CategoryDAO;
 import com.linan.tmall.pojo.Category;
+import com.linan.tmall.util.Page4Navigator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +16,15 @@ import java.util.List;
 public class CategoryService {
     @Autowired
     CategoryDAO categoryDAO;  //自动装配到底是啥意思
+
+    public Page4Navigator<Category> list(int start, int size, int navigatePages) {
+        System.out.println("start="+start+",size="+size);
+        Sort sort = new Sort(Sort.Direction.DESC, "id");
+        Pageable pageable = new PageRequest(start, size, sort);
+        Page pageFromJPA = categoryDAO.findAll(pageable);
+
+        return new Page4Navigator<>(pageFromJPA, navigatePages);
+    }
 
     public List<Category> list() {
         Sort sort = new Sort(Sort.Direction.DESC, "id");
--- src/main/java/com/linan/tmall/web/CategoryController.java
+++ src/main/java/com/linan/tmall/web/CategoryController.java
@@ -2,8 +2,10 @@ package com.linan.tmall.web;
 
 import com.linan.tmall.pojo.Category;
 import com.linan.tmall.service.CategoryService;
+import com.linan.tmall.util.Page4Navigator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,7 +16,12 @@ public class CategoryController {//专门用来提供restful服务的控制器�
     CategoryService categoryService;
 
     @GetMapping("/categories")
-    public List<Category> list() throws Exception {
-        return categoryService.list(); //最后会返回json数据
+    public Page4Navigator<Category> list(
+            @RequestParam(value = "start", defaultValue = "0") int start,
+            @RequestParam(value = "size", defaultValue = "5") int size
+    ) throws Exception {
+        start = start<0 ? 0 : start;
+        Page4Navigator<Category> page = categoryService.list(start,size,5);
+        return page; //最后会返回json数据
     }
 }
